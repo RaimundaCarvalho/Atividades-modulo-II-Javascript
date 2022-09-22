66f647c7896e62ebfa67fa1ceedce9a85e1b1dbf
RaimundaCarvalho <trindadebrito37@gmail.com>
2022-09-22 11:04:30 -0300

    atividades javascript

---
+++ Aula_02/index.js
@@ -0,0 +1,18 @@
+function votar(idade){
+    if (idade>=18 && idade<60){
+        console.log("Você deve votar")
+    
+    } else if(idade >=16 && idade <18 || idade>=70){
+        console.log("Você pode votar, mas não é obrigatório")
+    } else{
+        console.log("Você não pode votar")
+    }
+
+}
+votar(18)
+
+
+
+
+
+
